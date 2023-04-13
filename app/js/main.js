6038147ce12967ee0b3bf989cd9f54e9cdbc75ee
Davyd Arutiunov <davidarutyunov77@gmail.com>
2023-04-13 23:42:15 +0200

    add piece of page, add tabs

--- app/js/main.js
+++ app/js/main.js
@@ -11,3 +11,47 @@
 		headerNavigation.classList.remove('active')
 	});
 })();
+
+(function() {
+	const tabItems = document.querySelectorAll('.expirience__tab-link');
+	const tabContent = document.querySelectorAll('.expirience__content-item');
+
+	tabItems.forEach((tab, index) => {
+		tab.addEventListener('click', (event) => {
+			event.preventDefault();
+
+			tabContent.forEach(content => {
+				content.classList.remove('active');
+			});
+
+			tabItems.forEach(tab => {
+				tab.classList.remove('active');
+			});
+
+			tabContent[index].classList.add('active');
+			tabItems[index].classList.add('active');
+		})
+	})
+})();
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
